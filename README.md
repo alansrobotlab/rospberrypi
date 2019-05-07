9f5bdc9cb8260ea52527a2cd8c7b650a9a710cb8
Alan Timm <alansrobotlab@gmail.com>
2019-05-06 18:47:25 -0700

    Initial Upload
    Initial Upload

---
+++ README.md
@@ -0,0 +1,116 @@
+___
+# ROSpberryPi
+
+#### A quick and dirty way of bootstrapping ROS and some additional tools onto a Raspberry Pi Zero / W.  (well, maybe not quick, it still takes more than an hour to complete the script and get everything installed)
+___
+## FAQ:
+#### Why?
+- Recent ROS distributions only support ROS on the Raspberry pi 2B or greater (all arm7l devices) and on Ubuntu Mate.  The Raspberry Pi Zero/W is an arm6l device, so you if you want ROS on it you must compile it yourself which is a bit of a pain.  So I put together all of my work in a separate repository so others can take advantage of a prebuilt ROS Melodic setup.
+- You can pick up a Pi Zero / W for a little as $5, which opens up all kinds of options for small projects
+- If you operate the Pi Zero as headless you can do all of your development work on another machine, and just push the code to the zero.  My current setup has:
+    - the pi zero w set up as an access point (using create_ap)
+    - connect to it from my laptop using ssh/putty.  
+    - the ros src directory is shared and you can connect to it from \\\\(whatever your pi zero w ip address is)\\ros
+    - open the shared folder in visual studio code
+#### How?
+ - I downloaded and configured all of the required sources and compiled them on a Raspberry Pi 3 (I wouldn't recommend trying to compile them directly on a zero).  Next I packaged up the installs using checkinstall and hosted them on my google drive.  Then I wrote a script which grabs everything necessary for a functional ROS Melodic install.
+#### What works?
+ - Almost everything!  (As far as I know)
+#### What doesn't work?
+ - rviz can't seem to load urdf/stl files without crashing (but you shouldn't be running these tools on the zero anyways)
+ - visual studio code will not work on a pi zero (it relies on electron, which only compiles for the arm7l, and the pi zero is an arm6l)
+#### Will this work on any Raspberry Pi?
+ - In theory this should work on any Raspian Stretch install from the zero and original A+ to the 3B+.
+#### Will this work on a Pi 2B/3B/3B+?
+ - In theory, but for those devices it would be much easier to just install ubuntu mate and use the official packages
+___
+## Requirements:
+ - A Raspberry Pi Zero (preferably a W)
+ - Raspbian Stretch (Desktop or Minimal)
+ - An Internet connection
+___
+## What this script installs:
+ - A compatible version of TinyXML
+ - A compatible precompiled arm6l version of OpenCV3
+ - A compatible precompiled arm6l version of 
+     - ROS Melodic Desktop
+     - ROS Melodic Perception (minus the pcl stuff)
+     - ROS Melodic Robot
+     - ROS Melodic Joy(stick)
+     - All Dependencies
+ - Tensorflow
+ - Increases Swap to 1GB
+ - GPIZero libraries
+ - Installs create_ap (ability to use raspi zero w as access point)
+ - Initializes a ROS workspace
+  - Sets up a SAMBA Fileshare for the /home/pi/ros/src directory
+ - Sets up all env variables for ROS melodic and your ros workspace
+___
+## How to set this up
+1. Download and install Raspbian Stretch (Desktop or Lite) onto a microsd card
+2. Log into the pi and set it up to access the internet
+    - with full desktop, just use the UI
+        https://www.raspberrypi.org/documentation/configuration/wireless/desktop.md
+    - with lite, using raspi-config
+        https://www.raspberrypi.org/documentation/configuration/wireless/wireless-cli.md
+    - with lite, by configuring two files in the /boot directory
+        https://www.raspberrypi.org/documentation/configuration/wireless/headless.md
+    - with lite, using a UART (going headless)
+        https://learn.adafruit.com/raspberry-pi-zero-creation
+3. If you're feeling adventurous, maybe overclock the zero a bit (I'm not recommending you do this and I'm not going to walk you through how -- but possible settings are)
+   ```sh
+    # https://www.raspberrypi.org/forums/viewtopic.php?t=177743#p1169042
+    # 10% overclock
+    arm_freq=1100
+    over_voltage=8
+    sdram_freq=500
+    sdram_over_voltage=2
+    force_turbo=1
+    boot_delay=1
+    dtparam=sd_overclock=100
+    ```
+4. Open up a command prompt and type in the following commands.
+    ```sh
+    git clone https://github.com/alansrobotlab/rosberrypizero
+    cd rosberrypizero
+    ```
+5.  Read through the install script so you understand what you're about to do. (You're not the type of person that runs random scripts you download from the internet are you?)
+    ```sh
+    nano bootstrap.sh
+    ```
+6. Run the bootstrap script and go get a cup of coffee.
+   ```sh
+   sudo sh bootstrap.sh
+   ```
+7. Review the results of the bootstrap script, looking for any errors
+8. Carefully read the last lines of the bootstrap script.
+    ```
+    OK, now there are a few more things that you need to do
+    before you can get started.
+    First, run the following command in a command prompt
+    to set the samba filesharing password
+    for user 'pi'\n (Must be at least 8 characters)
+    
+    sudo smbpasswd -a pi
+    
+    
+    Next, disconnect from your wifi network and run
+    the following command to set up your pi as an access point
+    
+    sudo iw wlan0 disconnect (to disconnect wifi)
+    sudo systemctl enable create_ap (set access point to begin automatically on startup)
+    sudo systemctl start create_ap (to start the create_ap access point)
+    
+    
+    Finally, run the following command to enable ssh, the camera and i2c
+    
+    sudo rc-gui (for the gui config tool) \n\
+    sudo raspi-config (for the command line tool) \n"
+   ```
+9.  http://wiki.ros.org/ROS/Tutorials
+10. If you ever want to grab an updated version of this repository
+    ```sh
+    cd ~/.rosberrypizero # whereever you cloned this repository
+    git pull
+    ```
+___
---
+++ bootstrap.sh
@@ -0,0 +1,289 @@
+#!/bin/bash
+
+message () {
+	echo ""
+	echo "-----------------------------------------------------------"
+	echo ""
+	echo "$1"
+	echo ""
+	echo "-----------------------------------------------------------"
+	echo ""
+}
+
+message "Attempting to stop the create_ap access point (if it's running)..."
+sudo systemctl stop create_ap
+
+message "Checking for internet connectivity..."
+wget -q --spider http://google.com
+
+if ! [ $? -eq 0 ]; then
+	message "\nROSpberryPi is not currently connected to the internet.\nPlease connect to the internet and run this script again\nExiting...\n\n"
+	exit
+fi
+
+message "Increasing swapfile to 1Gb..."
+sudo dphys-swapfile swapoff
+sudo cp install/dphys-swapfile /etc/dphys-swapfile
+sudo dphys-swapfile swapon
+
+message "Checking for updates..."
+sudo apt update
+sudo apt upgrade -y
+
+message "Installing drmngr..."
+sudo apt install -y \
+	dirmngr
+
+message "Adding ros pathes to ~/.bashrc..."
+if ! grep "source /opt/ros/melodic/setup.bash" ~/.bashrc
+	then echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+fi
+
+message "Adding ros repositories..."
+if ! grep "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" /etc/apt/sources.list.d/ros-latest.list
+	then sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+fi
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+# removed, headmelted build of code-oss does not run on pi zero
+# message "Adding codebuilds key..."
+# sudo wget -qO - https://packagecloud.io/headmelted/codebuilds/gpgkey | sudo apt-key add -;
+
+message "Checking for updates again after repositories were added..."
+sudo apt update
+sudo apt upgrade -y
+
+message "Installing ROS dependencies..."
+sudo apt install -y \
+	python-rosdep \
+	python-rosinstall-generator \
+	python-wstool \
+	python-rosinstall \
+	build-essential
+
+message "Running rosdep init and update..."
+sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
+sudo rosdep init
+rosdep update
+
+message "Installing all opencv3 dependencies..."
+sudo apt install -y \
+	build-essential \
+	cmake \
+	pkg-config \
+	libjpeg-dev \
+	libtiff5-dev \
+	libjasper-dev \
+	libavcodec-dev \
+	libavformat-dev \
+	libswscale-dev \
+	libv4l-dev \
+	libxvidcore-dev \
+	libx264-dev \
+	libgtk2.0-dev \
+	libgtk-3-dev \
+	libatlas-base-dev \
+	gfortran \
+	python3-dev \
+	python3-pip \
+	libhdf5-100 \
+	libhdf5-serial-dev \
+	libhdf5-dev \
+	libhdf5-cpp-100 \
+	caffe-cpu
+
+message "pip3 install of tensorflow..."
+sudo pip3 install numpy scipy tensorflow
+
+# All ROS Melodic Stretch Install Dependencies
+
+message "Installing ROS Melodic Desktop dependencies..."
+sudo apt install -y\
+	python-opencv \
+	python-matplotlib \
+	python-sip-dev \
+	libeigen3-dev \
+	libqt5widgets5 \
+	libapr1-dev \
+	libaprutil1-dev \
+	libtinyxml2-dev \
+	qtbase5-dev \
+	liblz4-dev \
+	sbcl \
+	tango-icon-theme \
+	libgl1-mesa-dev \
+	libglu1-mesa-dev \
+	python-nose \
+	python-imaging \
+	libcurl4-openssl-dev \
+	curl \
+	libassimp-dev \
+	liburdfdom-dev \
+	python-yaml \
+	libbz2-dev \
+	python-lxml \
+	cmake \
+	uuid-dev \
+	python-crypto \
+	python-empy \
+	python-netifaces \
+	liburdfdom-headers-dev \
+	libgtest-dev \
+	libpython2.7-stdlib \
+	python-paramiko \
+	python-coverage \
+	python-pyqt5.qtopengl \
+	google-mock \
+	libboost-all-dev \
+	liblog4cxx-dev \
+	libyaml-cpp-dev \
+	qt5-qmake \
+	python-mock \
+	python-wxtools \
+	python-cairo \
+	python-dev \
+	libssl-dev \
+	python-numpy \
+	libgtk2.0-dev \
+	python-rospkg \
+	libopencv-dev \
+	libgpgme-dev \
+	libogg-dev \
+	hddtemp \
+	libtinyxml-dev \
+	libpoco-dev \
+	libconsole-bridge-dev \
+	graphviz \
+	libqt5opengl5 \
+	libqt5opengl5-dev \
+	python-gnupg \
+	libtheora-dev \
+	python-opengl \
+	libcppunit-dev \
+	python-psutil \
+	libogre-1.9-dev \
+	python-defusedxml \
+	python-pygraphviz \
+	python-pydot \
+	python-catkin-pkg \
+	libqt5gui5 \
+	libqt5core5a \
+	python-rosdep \
+	python-pyqt5.qtwebkit \
+	joystick \
+	pyqt5-dev \
+	python-pyqt5 \
+	python-pyqt5.qtsvg \
+	python-sip-dev \
+	qtbase5-dev \
+	pkg-config
+
+message "Installing Samba filesharing tools..."
+sudo apt-get install -y \
+	samba \
+	samba-common-bin \
+	smbclient cifs-utils
+
+#message "Installing adafruit servokit..."
+#sudo pip3 install adafruit-circuitpython-servokit
+
+message "Installing gpizero libraries..."
+sudo apt install -y \
+	python-gpiozero \
+	python3-gpiozero
+
+message "Installing ROS compatible version of tinyxml2..."
+# required for rospack,
+# https://answers.ros.org/question/278733/rospack-find-throws-exception-error/?answer=279421#post-id-279421
+cd install
+git clone https://github.com/leethomason/tinyxml2.git
+cd tinyxml2
+mkdir build
+cd build
+cmake ..
+make 
+sudo make install
+cd ..
+
+message "Installing precompiled arm6l compatible version of opencv3 (raspi 0,1,2 compatible)..."
+#if [ ! -f ./install/opencv.deb]; then
+	curl -L -o opencv.deb "https://drive.google.com/uc?export=download&id=1i8RBgxMXuCMxyIPgjWoHWeX7xmEmuM23"
+#fi
+sudo dpkg -i --force-all ./opencv.deb
+
+message "Installing precompiled arm6l compatible version of ros melodic + perception + robot + joy(stick)..."
+#if [ ! -f ./install/ros_desktop.tar.bz2]; then
+	curl -L -o ros_desktop.tar.bz2 "https://drive.google.com/uc?export=download&id=1ffIgOm6M6TicZbAcWjK7va_A33rBHrs4"
+#fi
+sudo tar xjf ./ros_desktop.tar.bz2 -C /
+# for some reason this is now required
+sudo cp /usr/local/lib/libtinyxml.so.7.0.1 /usr/ros/melodic/lib/libtinyxml.so.7
+
+#message "Installing precompiled version of Visual Studio Code (vscode-arm)...\n\
+# (although this currently will not run on the Zero because of electron)"
+# https://github.com/stevedesmond-ca/vscode-arm
+# https://www.raspberrypi.org/forums/viewtopic.php?t=191342#p1440607
+#cd ./install
+#wget https://github.com/stevedesmond-ca/vscode-arm/releases/download/1.28.2/vscode-1.28.2.deb
+#sudo apt install ./vscode-1.28.2.deb
+#cd ~/toddthequad
+
+message "Installing create_ap (Raspi as a wifi access point)..."
+# https://github.com/oblique/create_ap
+sudo apt install -y \
+	util-linux \
+	procps \
+	hostapd \
+	iproute2 \
+	haveged \
+	dnsmasq \
+	iptables
+
+git clone https://github.com/oblique/create_ap
+cd create_ap
+sudo make install
+sudo create_ap -n wlan0 pi rospberry --mkconfig /etc/create_ap.conf
+#sudo systemctl enable create_ap
+#sudo systemctl start create_ap
+
+message "Initializing ROS workspace..."
+mkdir /home/pi/ros
+mkdir /home/pi/ros/src
+
+message "Configuring Samba filesharing..."
+sudo systemctl stop smbd
+sudo cp ./smb.conf /etc/samba/smb.conf
+sudo systemctl start smbd
+
+#message "Now, please enter in a samba (windows filesharing) password for use"
+#sudo smbpasswd -a pi
+#sudo systemctl start smbd
+
+message "\
+\n\
+DONE!!!! \n\
+\n\
+\n\
+OK, now there are a few more things that you need to do \n\
+ before you can get started. \n\
+ \n\
+ \n\
+First, run the following command in a command prompt \n\
+ to set the samba filesharing password \n\
+ for user 'pi'\n (Must be at least 8 characters) \n\
+ \n\
+ sudo smbpasswd -a pi \n\
+ \n\
+ \n\
+Next, disconnect from your wifi network and run \n \
+ the following command to set up your pi as an access point \n\
+ \n\
+ sudo iw wlan0 disconnect (to disconnect wifi) \n\
+ sudo systemctl enable create_ap (set access point to begin automatically on startup) \n\
+ sudo systemctl start create_ap (to start the create_ap access point)\n\
+ \n\
+ \n\
+Finally, run the following command to enable ssh, the camera and i2c \n\
+ \n\
+ sudo rc-gui (for the gui config tool) \n\
+ sudo raspi-config (for the command line tool) \n"
